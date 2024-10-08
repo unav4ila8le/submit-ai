41bafaa0b6dddca20d0681407e473b7a377b5c23
unav4ila8le <leonardobalduzzi@me.com>
2024-10-08 16:49:46 +0900

    lucide icons

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,7 +1,19 @@
+import { Bot } from "lucide-react";
 import React from "react";
 
 const PageHeader: React.FC = () => {
-  return <div>this is the header</div>;
+  return (
+    <div className="bg-white">
+      <div className="mx-auto w-full max-w-screen-xl items-center justify-between p-4">
+        <div className="flex flex-row items-center gap-1">
+          <Bot />
+          <span className="text-primary mt-1 font-semibold uppercase">
+            Submit.ai
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageHeader;
