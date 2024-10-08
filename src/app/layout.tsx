9b3775802eb3039733e5949c5f5ef9fa1db7ff35
unav4ila8le <leonardobalduzzi@me.com>
2024-10-09 02:21:20 +0900

    theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 import { cn } from "@/lib/utils";
 import PageHeader from "@/components/page-header";
+import { ThemeProvider } from "@/components/context/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,10 +26,17 @@ export default function RootLayout({
       <body
         className={cn("min-h-screen font-sans antialiased", inter.variable)}
       >
-        <header>
-          <PageHeader />
-        </header>
-        <main>{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header>
+            <PageHeader />
+          </header>
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,16 +1,18 @@
 import { Bot } from "lucide-react";
 import React from "react";
+import { ModeToggle } from "./mode-toggle";
 
 const PageHeader: React.FC = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto w-full max-w-screen-xl items-center justify-between p-4">
+      <div className="mx-auto flex w-full max-w-screen-xl flex-row items-center justify-between p-4">
         <div className="flex flex-row items-center gap-1">
           <Bot />
-          <span className="text-primary mt-1 font-semibold uppercase">
+          <span className="mt-1 font-semibold uppercase text-primary">
             Submit.ai
           </span>
         </div>
+        <ModeToggle />
       </div>
     </div>
   );
