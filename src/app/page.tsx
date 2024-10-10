1ea089c8a70ffd730f70d99f1b7198275204c1ca
unav4ila8le <leonardobalduzzi@me.com>
2024-10-10 17:14:08 +0900

    landingpage reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import LogoCarousel from "@/components/logo-carousel";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Bot, CheckCircle2, Pencil } from "lucide-react";
+import { Bot, CheckCircle2, Pencil, Star } from "lucide-react";
 
 export default function Home() {
   return (
@@ -23,19 +23,19 @@ export default function Home() {
         </Button>
         <div className="mt-6 flex flex-row items-center justify-center gap-2">
           <div className="flex flex-row items-center -space-x-2">
-            <Avatar className="h-8 w-8 ring-2 ring-background">
+            <Avatar className="h-8 w-8 outline outline-2 outline-background">
               <AvatarImage
                 src="https://i.pravatar.cc/64"
                 alt="User profile picture"
               />
             </Avatar>
-            <Avatar className="h-8 w-8 ring-2 ring-background">
+            <Avatar className="h-8 w-8 outline outline-2 outline-background">
               <AvatarImage
                 src="https://i.pravatar.cc/64"
                 alt="User profile picture"
               />
             </Avatar>
-            <Avatar className="h-8 w-8 ring-2 ring-background">
+            <Avatar className="h-8 w-8 outline outline-2 outline-background">
               <AvatarImage
                 src="https://i.pravatar.cc/64"
                 alt="User profile picture"
@@ -45,7 +45,9 @@ export default function Home() {
           <p>Join 500,000+ writers</p>
         </div>
       </div>
-      <div className="mt-10 h-[640px] bg-slate-200">tool goes here</div>
+      <div className="mt-10 h-[640px] bg-slate-200 dark:bg-slate-700">
+        tool goes here
+      </div>
       <div className="mt-12 text-center">
         <div className="mb-4 flex flex-row justify-center gap-2">
           <CheckCircle2 className="text-slate-500" />
@@ -55,37 +57,143 @@ export default function Home() {
         </div>
         <LogoCarousel />
       </div>
-      <div className="mt-40 text-center">
-        <Badge
-          size="lg"
-          className="gap-2 bg-primary-200 font-bold uppercase text-primary"
-        >
-          <Bot /> Complete AI Toolkit
-        </Badge>
-        <h2 className="mt-4 text-5xl sm:text-6xl">
-          Ultimate Writing Assistant
-        </h2>
-        <p className="mt-6 text-xl">
-          Features built to enhance your writing capabilities and save you time.
-        </p>
-        <div className="mt-16 grid grid-cols-1 gap-10 text-start md:grid-cols-2">
+      <div className="mt-40">
+        <div className="mx-auto max-w-4xl text-center">
+          <Badge
+            size="lg"
+            className="gap-2 bg-primary/30 font-bold uppercase text-primary"
+          >
+            <Bot /> Complete AI Toolkit
+          </Badge>
+          <h2 className="mt-4 text-5xl sm:text-6xl">
+            Ultimate Writing Assistant
+          </h2>
+          <p className="mt-6 text-xl">
+            Features built to enhance your writing capabilities and save you
+            time.
+          </p>
+        </div>
+        <div className="mt-16 grid grid-cols-1 gap-10 md:grid-cols-2">
           <div>
-            <div className="h-80 bg-slate-200"></div>
+            <div className="h-80 bg-slate-200 dark:bg-slate-700"></div>
             <h3 className="mt-6 text-3xl">AI Humanizer & Paraphraser</h3>
-            <p className="mt-3">
+            <p className="mt-2">
               Humanize AI-generated content to bypass AI detectors like GPTZero,
               TurnItIn, and more. Ensure your content is undetectable by all AI
               detectors.
             </p>
           </div>
           <div>
-            <div className="h-80 bg-slate-200"></div>
+            <div className="h-80 bg-slate-200 dark:bg-slate-700"></div>
             <h3 className="mt-6 text-3xl">AI Writer</h3>
-            <p className="mt-3">
+            <p className="mt-2">
               Organize and generate your next paper, article, or more within
               seconds, with support for custom in-text citations from the
               internet.
             </p>
+          </div>
+          <div>
+            <div className="h-80 bg-slate-200 dark:bg-slate-700"></div>
+            <h3 className="mt-6 text-3xl">AI Detector</h3>
+            <p className="mt-2">
+              Check your content for free to see if it gets picked up by AI
+              detectors with a 99.8% accuracy rate. Our AI detector has
+              industry-leading accuracy.
+            </p>
+          </div>
+          <div>
+            <div className="h-80 bg-slate-200 dark:bg-slate-700"></div>
+            <h3 className="mt-6 text-3xl">Quick Export</h3>
+            <p className="mt-2">
+              Quickly export your generated content to multiple supported
+              formats of your choice such as Google Docs, Microsoft Word, etc.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-40">
+        <div className="mx-auto max-w-4xl text-center">
+          <Badge
+            size="lg"
+            className="gap-2 bg-yellow-500/30 font-bold uppercase text-yellow-500"
+          >
+            <Star /> Reviews
+          </Badge>
+          <h2 className="mt-4 text-5xl sm:text-6xl">
+            Join 500,000+ writers using Phrasly
+          </h2>
+          <p className="mt-6 text-xl">
+            Phrasly has helped write over 250 million words. From academic
+            essays, journals, to top-ranking articles - all bypassing AI
+            detectors.
+          </p>
+        </div>
+        <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-3">
+          <div className="aspect-[9/16] max-h-[80vh] w-full bg-slate-200 dark:bg-slate-700"></div>
+          <div className="aspect-[9/16] max-h-[80vh] w-full bg-slate-200 dark:bg-slate-700"></div>
+          <div className="aspect-[9/16] max-h-[80vh] w-full bg-slate-200 dark:bg-slate-700"></div>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-3xl grid-cols-4 items-start gap-8 xl:max-w-full">
+          <div className="col-span-4 rounded-2xl border bg-card p-6 shadow-lg xl:col-span-2">
+            <blockquote className="text-lg font-semibold text-slate-900 dark:text-slate-100 sm:text-xl">
+              “I used to spend hours manually rewriting my text to bypass AI
+              detectors, but Phrasly has simplified the entire process for me
+              and gives me the peace of mind I need. Thank you Phrasly!”
+            </blockquote>
+            <div className="mt-5 flex flex-row items-center gap-2 xl:mt-16">
+              <Avatar className="h-10 w-10">
+                <AvatarImage
+                  src="https://i.pravatar.cc/64"
+                  alt="User profile picture"
+                />
+              </Avatar>
+              <div>
+                <div className="font-semibold text-slate-900 dark:text-slate-100">
+                  Jordan Brown
+                </div>
+                <div>@thejordanbrown</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-4 rounded-2xl border bg-card p-6 shadow-lg md:col-span-2 xl:order-first xl:col-span-1">
+            <blockquote className="text-lg text-slate-700 dark:text-slate-300">
+              “I didn't think Phrasly would be able to create the quality of
+              content I needed. Now it's part of my daily workflow. I love it.”
+            </blockquote>
+            <div className="mt-5 flex flex-row items-center gap-2">
+              <Avatar className="h-10 w-10">
+                <AvatarImage
+                  src="https://i.pravatar.cc/64"
+                  alt="User profile picture"
+                />
+              </Avatar>
+              <div>
+                <div className="font-semibold text-slate-900 dark:text-slate-100">
+                  Alex Harper
+                </div>
+                <div>@aharper_</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-4 rounded-2xl border bg-card p-6 shadow-lg md:col-span-2 xl:col-span-1">
+            <blockquote className="text-lg text-slate-700 dark:text-slate-300">
+              “Phrasly has been a game-changer for me as a busy college student.
+              It saves me so much time on essay writing by using AI!”
+            </blockquote>
+            <div className="mt-5 flex flex-row items-center gap-2">
+              <Avatar className="h-10 w-10">
+                <AvatarImage
+                  src="https://i.pravatar.cc/64"
+                  alt="User profile picture"
+                />
+              </Avatar>
+              <div>
+                <div className="font-semibold text-slate-900 dark:text-slate-100">
+                  Jordan Brown
+                </div>
+                <div>@thejordanbrown</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
