220bb3e75215d295df0aaa5f356a48a483730b47
unav4ila8le <leonardobalduzzi@me.com>
2024-10-11 16:17:10 +0900

    pricing colors

--- src/components/pricing-table.tsx
+++ src/components/pricing-table.tsx
@@ -5,9 +5,9 @@ import { Button } from "./ui/button";
 
 const PricingTable = () => {
   return (
-    <div className="grid grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 items-center gap-6 sm:grid-cols-2">
       <div className="rounded-2xl border bg-card p-6 shadow-lg">
-        <h3 className="text-2xl">Free</h3>
+        <h3 className="text-2xl !text-slate-500">Free</h3>
         <p className="mt-3">
           The flexible option for individuals who want to try our AI tools.
         </p>
@@ -42,8 +42,8 @@ const PricingTable = () => {
           </li>
         </ul>
       </div>
-      <div className="rounded-2xl border bg-card p-6 shadow-lg">
-        <h3 className="text-2xl">Free</h3>
+      <div className="rounded-2xl border-2 border-primary bg-card p-6 shadow-lg">
+        <h3 className="text-2xl">Unlimited</h3>
         <p className="mt-3">
           The flexible option for individuals who want to try our AI tools.
         </p>
@@ -56,24 +56,28 @@ const PricingTable = () => {
         <p className="mt-2 text-xs text-slate-500">
           billed as $131.88 / year after trial
         </p>
-        <Button size="xl" variant="outline" className="mt-6 w-full">
-          <CreditCard className="mr-2" /> Start Free Trial
+        <Button size="xl" className="mt-6 w-full">
+          <CreditCard className="mr-2" /> Upgrade Plan
         </Button>
         <ul className="mt-6 flex flex-col gap-4">
           <li className="flex flex-row gap-2">
-            <CheckCircle2 className="h-6 w-6" />
+            <CheckCircle2 className="h-6 w-6 text-primary" />
             <p>550 AI Humanization words</p>
           </li>
           <li className="flex flex-row gap-2">
-            <CheckCircle2 className="h-6 w-6" />
+            <CheckCircle2 className="h-6 w-6 text-primary" />
             <p>550 AI Humanization words</p>
           </li>
           <li className="flex flex-row gap-2">
-            <CheckCircle2 className="h-6 w-6" />
+            <CheckCircle2 className="h-6 w-6 text-primary" />
             <p>550 AI Humanization words</p>
           </li>
           <li className="flex flex-row gap-2">
-            <CheckCircle2 className="h-6 w-6" />
+            <CheckCircle2 className="h-6 w-6 text-primary" />
+            <p>550 AI Humanization words</p>
+          </li>
+          <li className="flex flex-row gap-2">
+            <CheckCircle2 className="h-6 w-6 text-primary" />
             <p>550 AI Humanization words</p>
           </li>
         </ul>
