d2f4797aaf400566bfb0783f468e295ea139f1b5
unav4ila8le <leonardobalduzzi@me.com>
2024-10-17 11:16:46 +0700

    button radius fixed

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md whitespace-nowrap text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -22,8 +22,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2 rounded-md",
-        sm: "h-9 px-3",
+        default: "h-10 px-4 py-2",
+        sm: "h-9 px-3 rounded-sm",
         lg: "h-11 rounded-lg px-4",
         xl: "h-14 rounded-xl px-5 text-lg",
         icon: "h-10 w-10",
